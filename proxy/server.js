d648d28618d381fef8e273638f9d674644037e3e
ANAIT <Anait@ANAITs-MacBook-Pro.local>
2019-04-13 14:39:49 -0700

    Rendering all four services, made some changes to class names and id's, created index.html and server.js files

---
+++ proxy/server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const httpProxy = require('http-proxy');
+const proxy = httpProxy.createProxyServer();
+const path = require('path');
+const http = require('http');
+
+const port = 7000;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/allReviews/item/:id', function(req, res) {
+  proxy.web(req, res, { target: 'http://localhost:9000' });
+});
+
+app.get('/allItems/item/:id', function(req, res) {
+  console.log('req.params.id in /allItems', req.params.id);
+  proxy.web(req, res, { target: 'http://localhost:9000' });
+});
+
+app.get('/items', (req, res) => {
+  proxy.web(req, res, { target: 'http://localhost:3000' });
+})
+
+app.get('/images', (req, res) => {
+  proxy.web(req, res, { target: 'http://localhost:3000' });
+})
+
+app.get('/benefits', (req, res) => {
+  proxy.web(req, res, { target: 'http://localhost:3000' });
+})
+
+app.get('/item-benefits', (req, res) => {
+  proxy.web(req, res, { target: 'http://localhost:3000' });
+})
+
+app.get('/test1', function (req, res) {
+  proxy.web(req, res, { target: 'http://localhost:6001' });
+});
+
+app.get('/test', (req, res) => {
+  proxy.web(req, res, { target: 'http://localhost:8080' });
+});
+
+app.listen(port, () => console.log('Server is running'))
